254ac8df752600e94a5b3ddd3eccdd40a69e7df7
AdenHoang <132239401+AdenHoang@users.noreply.github.com>
2024-04-23 00:07:32 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+private ArrayList<String> aden;
+  public RandomStringChooser(String[] wordArray){
+   for (int i =0; i<wordArray.length;i++){
+     aden.add(wordArray[i]);
+   }
+  }
+  public String getNext(){
+  if(aden.size()>0)
+  {
+    return aden.remove((int)(Math.random()*aden.size()));
+  }
+    return "NONE";
+  }
 }
